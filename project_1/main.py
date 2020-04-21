44a88963e9407d0eab73b682e84341e5d6b8af9f
ticet11 <BriKozub@gmail.com>
2020-04-21 14:31:52 -0600

    Initial Commit, solved project 1, basic readme

---
+++ project_1/main.py
@@ -0,0 +1,18 @@
+"""
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+"""
+
+
+def fizz_buzz():
+    sum_list = []
+    max_number = input('What should the max number be?:')
+    for num in range(int(max_number)):
+        if num % 3 == 0 or num % 5 == 0:
+            sum_list.append(num)
+
+    print(sum(sum_list))
+
+
+fizz_buzz()
---
+++ readme.md
@@ -0,0 +1,1 @@
+My solutions to the projecteuler.net problems, using python.
